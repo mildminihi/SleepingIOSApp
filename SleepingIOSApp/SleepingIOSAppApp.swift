514e7e9870c41c458b90b4721ab40c2c2a7f906c
supanat wanroj <supanat.wanr@kkpfg.com>
2023-07-31 18:26:25 +0700

    Initial Commit

---
+++ SleepingIOSApp/SleepingIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepingIOSAppApp.swift
+//  SleepingIOSApp
+//
+//  Created by Supanat Wanroj on 31/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SleepingIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
